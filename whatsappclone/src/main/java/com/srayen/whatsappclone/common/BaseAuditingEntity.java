c423bf6f342d8f98a61ce3b1f26f03faa954c949
S Ray <selmen.rayen.92@gmail.com>
2025-09-14 14:27:51 +0100

    create BaseAuditingEntity

---
+++ whatsappclone/src/main/java/com/srayen/whatsappclone/common/BaseAuditingEntity.java
@@ -0,0 +1,32 @@
+package com.srayen.whatsappclone.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseAuditingEntity {
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedBy
+    @Column(name = "last_modified_date", insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
